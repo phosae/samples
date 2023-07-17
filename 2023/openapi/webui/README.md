63e70699ef840477824ebad66d28829d087e0d4a
zengxu <zengxu@qiniu.com>
2023-07-17 18:17:32 +0800

    fix localhost bug

--- 2023/openapi/webui/README.md
+++ 2023/openapi/webui/README.md
@@ -3,8 +3,9 @@
 
 ## play
 run in docker and visit http://localhost:8000 in browser
+
 ```shell
-docker run -it --rm -p 8000:8000 --entrypoint bash zengxu/openapi-ui
+docker run --rm -p 8000:8000 zengxu/openapi-ui
 ```
 
 ## dev
--- 2023/openapi/webui/web/vite.config.js
+++ 2023/openapi/webui/web/vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       // http://localhost:5173/view -> http://localhost:8000/view
-      "/view": "http://localhost:8000",
-      "/uis": "http://localhost:8000",
-      "/apifiles": "http://localhost:8000",
+      // "/view": "http://localhost:8000",
+      // "/uis": "http://localhost:8000",
+      // "/apifiles": "http://localhost:8000",
     }
   },
   plugins: [vue()],
